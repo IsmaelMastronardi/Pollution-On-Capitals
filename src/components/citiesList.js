3ef77046965d8c380834bbf72a6fcf63da8bd8bb
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-09-06 18:03:20 -0300

    add styles for single city

--- src/components/citiesList.js
+++ src/components/citiesList.js
@@ -1,56 +1,21 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import cityIcon from '../assets/icons/city-solid.svg';
 
-// export const CityList = () => {
-//   const { cities, isLoading, filter } = useSelector((store) => (store.citiesStore));
-//   if (isLoading === 'false') {
-//     if (filter === 'all') {
-//       return (
-//         <>
-//           {cities.map((city) =>
-//     <Link to={`/${city.name}`} key={city.name}><SingleCity key={city.name} obj={city} /></Link>)}
-//         </>
-//       );
-//     }
-//     const filteredArr = cities.filter((el) => el.data.main.aqi === Number(filter));
-//     return (
-//       <>
-//         {filteredArr.map((city) => <SingleCity key={city.name} obj={city} />)}
-//       </>
-//     );
-//   }
-//   return (
-//     <p>Loading...</p>
-//   );
-// };
 export const CityList = () => {
   const { cities, isLoading, filter } = useSelector((store) => (store.citiesStore));
   if (isLoading === 'false') {
     if (filter === 'all') {
-      // return (
-      //   <section className="city-section">
-      //     {cities.map((city, index) => (
-      //       <div key={city.name} className={index % 2 === 0 ? 'city-div' : 'city-div-ligth'}>
-      //         <Link to={`/${city.name}`} key={city.name} className="city-link">
-      //           {city.name}
-      //         </Link>
-      //         <p>
-      //           AQI:
-      //           {city.data.main.aqi}
-      //         </p>
-      //       </div>
-      //     ))}
-      //   </section>
-      // );
       return (
         <section className="city-section">
           {cities.map((city, index) => (
-            <Link to={`/${city.name}`} key={city.name} className={index % 2 === 0 ? 'city-div' : 'city-div-ligth'}>
-              <div key={city.name}>
-                <p>
-                  {city.name}
+            <Link to={`/${city.name}`} key={city.name} className={index % 2 === 0 ? 'city-div city-div-dark' : 'city-div city-div-ligth'}>
+              <img src={cityIcon} alt="" className="city-icon" />
+              <div key={city.name} className="city-text">
+                <p className="city-name">
+                  {city.name.toUpperCase()}
                 </p>
-                <p>
+                <p className="city-name">
                   AQI:
                   {city.data.main.aqi}
                 </p>
--- src/components/singleCity.js
+++ src/components/singleCity.js
@@ -2,16 +2,11 @@
 // import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import cityIcon from '../assets/icons/city-solid.svg';
+import rigthArrow from '../assets/icons/circle-right-regular.svg';
+import '../styles/title.css';
+import '../styles/city.css';
 
-// const SingleCity = ({ obj }) => (
-//   <>
-//     <p>{obj.name}</p>
-//     <h3>{obj.data.main.aqi}</h3>
-//     <ul>
-//       <li>{obj.data.components.co}</li>
-//     </ul>
-//   </>
-// );
 const SingleCity = () => {
   const { name } = useParams();
   const { cities } = useSelector((store) => (store.citiesStore));
@@ -19,32 +14,80 @@ const SingleCity = () => {
   const keys = Object.keys(singleCity.data.components);
   const compArr = [];
   keys.forEach((key) => {
-    compArr.push(`${key}: ${singleCity.data.components[key]}`);
+    compArr.push({
+      key,
+      val: singleCity.data.components[key],
+    });
+    // compArr.push(`${key}: ${singleCity.data.components[key]}`);
   });
   return (
-    <article>
-      <div className="single-city-title-div">
-        <p>
-          city:
-          {' '}
-          {singleCity.name}
-        </p>
-      </div>
+    <>
+      <section>
+        <div className="title-section">
+          <div className="width-50">
+            <img src={cityIcon} className="earth-logo" alt="" />
+          </div>
+          <div className="width-50">
+            <p className="title-p">
+              city:
+            </p>
+            <p className="title-p">
+              {singleCity.name}
+            </p>
+          </div>
+        </div>
+      </section>
       <div>
-        <p>Polution Breackdown</p>
+        <p className="subtitle">POLUTION BREACKDOWN</p>
       </div>
-      <div>
-        <ul>
-          {compArr.map((el) => <p key={el}>{el}</p>)}
-        </ul>
-      </div>
-    </article>
+      <article>
+        <section>
+          <ul className="components-list">
+            {compArr.map((el, index) => (
+              <li key={el.key} className={index % 2 === 0 ? 'component-item city-div-dark' : 'component-item item-ligth'}>
+                <p className="component-name">{el.key.toUpperCase()}</p>
+                <div className="city-item-rigth">
+                  <p className="component-value">{el.val}</p>
+                  <img src={rigthArrow} alt="" className="rigth-arrow" />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </article>
+
+    </>
   );
 };
 
-// SingleCity.propTypes = {
-//   // eslint-disable-next-line react/forbid-prop-types
-//   obj: PropTypes.objectOf(PropTypes.any).isRequired,
-// };
+// <Table striped bordered hover>
+// <thead>
+//   <tr>
+//     <th>#</th>
+//     <th>First Name</th>
+//     <th>Last Name</th>
+//     <th>Username</th>
+//   </tr>
+// </thead>
+// <tbody>
+//   <tr>
+//     <td>1</td>
+//     <td>Mark</td>
+//     <td>Otto</td>
+//     <td>@mdo</td>
+//   </tr>
+//   <tr>
+//     <td>2</td>
+//     <td>Jacob</td>
+//     <td>Thornton</td>
+//     <td>@fat</td>
+//   </tr>
+//   <tr>
+//     <td>3</td>
+//     <td colSpan={2}>Larry the Bird</td>
+//     <td>@twitter</td>
+//   </tr>
+// </tbody>
+// </Table>
 
 export default SingleCity;

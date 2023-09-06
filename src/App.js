a37eadc83b29c717fc99dde274e6bfbfccdca007
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-09-06 15:23:56 -0300

    add arrow

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import PollutionApp from './components/pollutionApp';
 import SingleCity from './components/singleCity';
+import arrow from './assets/icons/chevron-left-solid.svg';
 
 function App() {
   return (
     <>
-      <Link to="/">Home</Link>
+      <Link to="/">
+        <button type="button" className="arrowBtn">
+          <img src={arrow} alt=" go back" className="arrow" />
+        </button>
+      </Link>
       <Routes>
         <Route path="/" element={<PollutionApp />} />
         <Route path="/:name" element={<SingleCity />} />

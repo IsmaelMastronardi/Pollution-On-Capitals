0dea73b4d49484a5e0586f20fcc54a16fe95c627
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-09-06 15:03:39 -0300

    add logo and styling

--- src/components/title.js
+++ src/components/title.js
@@ -1,15 +1,29 @@
 import { useSelector } from 'react-redux';
+import earth from '../assets/icons/earth-americas-solid.svg';
+import '../styles/title.css';
 
 const AppTitle = () => {
   const { cities, filter } = useSelector((store) => (store.citiesStore));
   return (
-    <section>
-      <p>
-        Selected Capitals:
-        {' '}
-        {filter === 'all' ? cities.length : cities.filter((el) => el.data.main.aqi === Number(filter)).length}
-      </p>
-    </section>
+    <>
+      <section className="title-section">
+        <div className="width-50">
+          <img src={earth} alt="" className="earth-logo" />
+        </div>
+        <div className="width-50">
+          <p className="title-p">
+            SELECTED CAPITALS :
+            {' '}
+          </p>
+          <p className="title-p">
+            {filter === 'all' ? cities.length : cities.filter((el) => el.data.main.aqi === Number(filter)).length}
+          </p>
+        </div>
+      </section>
+      <div>
+        <p className="subtitle">POLLUTION IN CAPITAL CITY</p>
+      </div>
+    </>
   );
 };
 
